3218282a372a3455bb4b9b1f1394df12be3e814b
hamada <hamada@MacBook-Air.local>
2020-06-05 14:27:51 +0200

    adding models

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ const http = require('http');
 const express = require('express');
 const mongoose = require('mongoose');
 
+// connect to database
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.log(`Mongoose connection error : ${err.message}`);
+});
+
+db.once('open', () => {
+    console.log('DB connected');
+});
+
+// models
+require('./models/User');
+require('./models/Chatroom');
+require('./models/Message');
+
 
 
 const app = express();
