18fdbfe43e1c3550953124569cacbbcfd868c199
hamada <hamadagh777@gmail.com>
2020-06-26 16:20:34 +0200

    adding chat board style

--- frontend/src/pages/dashboard/components/ChatBoard.js
+++ frontend/src/pages/dashboard/components/ChatBoard.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "../dasboard.css";
 
 function ChatBoard() {
-  return <div></div>;
+  return (
+    <div className="chat-board">
+      <h3>Chat title</h3>
+    </div>
+  );
 }
 
 export default ChatBoard;
