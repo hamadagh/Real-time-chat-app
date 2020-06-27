806fb85f94abd3b3cc22106715f3a41d953e1be9
hamada <hamadagh777@gmail.com>
2020-06-28 01:55:55 +0200

    installing axios

--- frontend/src/pages/auth/SignUp.js
+++ frontend/src/pages/auth/SignUp.js
@@ -31,15 +31,32 @@ function SignUp() {
   const emailRef = React.createRef();
   const passwordRef = React.createRef();
 
-  const SignUserUp = () => {
+  const signUserUp = () => {
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
+
+    axios
+      .post("http://localhost:8000/register", {
+        email,
+        password,
+      })
+      .then((response) => {
+        console.log("user created !!");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
     <div className="sign-up">
       <h2>Sign up</h2>
-      <form className={classes.root} noValidate autoComplete="off">
+      <form
+        className={classes.root}
+        noValidate
+        autoComplete="off"
+        onSubmit={signUserUp}
+      >
         <TextField
           id="outlined-basic"
           label="Email"
@@ -54,10 +71,16 @@ function SignUp() {
           className="input-field"
           ref={passwordRef}
         />
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="primary"
+          onClick={signUserUp}
+        >
+          Create Account
+        </Button>
       </form>
-      <Button className={classes.button} variant="contained" color="primary">
-        Create Account
-      </Button>
+
       <p>
         Have an account?<a>Log in</a>
       </p>
