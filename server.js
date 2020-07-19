ae25ce5bfb88367ee8c311b98113ea9f3e1c1e64
hamada <hamadagh777@gmail.com>
2020-07-19 13:58:58 +0200

    installing socket io on server side

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
+const socket = require("socket.io");
 const dotenv = require("dotenv");
 dotenv.config();
 
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // connect to database
-mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 
-db.on('error', (err) => {
-    console.log(`Mongoose connection error : ${err.message}`);
+db.on("error", (err) => {
+  console.log(`Mongoose connection error : ${err.message}`);
 });
 
-db.once('open', () => {
-    console.log('DB connected');
+db.once("open", () => {
+  console.log("DB connected");
 });
 
 // models
-require('./models/User');
-require('./models/Chatroom');
-require('./models/Message');
-
-
+require("./models/User");
+require("./models/Chatroom");
+require("./models/Message");
 
 const app = require("./app");
 
 const PORT = 3001 || process.env.PORT;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+const io = socket(server);
